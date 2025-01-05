cc699ae65badceb63e64bfd98dd522d6c94b123b
PablooOtavio <pabloootaviooliveira@gmail.com>
2025-01-05 20:37:26 -0300

    enhance `/status` endpoint

--- src/pages/api/v1/status/index.js
+++ src/pages/api/v1/status/index.js
@@ -1,9 +1,34 @@
 import database from "src/infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 'Hello, World' as message;");
-  console.log(result.rows);
-  response.status(200).send(result);
+  const databaseName = process.env.POSTGRES_DB;
+  const updatedAt = new Date().toISOString();
+
+  const versionResult = await database.query("SHOW server_version;");
+  const maxConnectionsResult = await database.query("SHOW max_connections;");
+  const openedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const versionValue = versionResult.rows[0].server_version;
+  const maxConnectionsValue = parseInt(
+    maxConnectionsResult.rows[0].max_connections,
+  );
+  const openedConnectionsValue = openedConnectionsResult.rows[0].count;
+
+  const responseObject = {
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: versionValue,
+        max_connections: maxConnectionsValue,
+        opened_connections: openedConnectionsValue,
+      },
+    },
+  };
+
+  response.status(200).send(responseObject);
 }
 
 export default status;
