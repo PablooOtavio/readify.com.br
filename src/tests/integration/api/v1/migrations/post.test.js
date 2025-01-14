ed870bc48568c8b54251b2b8f9f3c39edc02ccd4
PablooOtavio <pabloootaviooliveira@gmail.com>
2025-01-14 17:09:19 -0300

    make `test` script more robust

--- src/tests/integration/api/v1/migrations/post.test.js
+++ src/tests/integration/api/v1/migrations/post.test.js
@@ -1,3 +1,11 @@
+import database from "src/infra/database";
+import orchestrator from "src/tests/orchestrator";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await database.query("DROP SCHEMA PUBLIC cascade; CREATE SCHEMA PUBLIC;");
+});
+
 test("POST to /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "POST",
---
+++ src/tests/orchestrator.js
@@ -0,0 +1,26 @@
+import retry from "async-retry";
+async function waitForAllServices() {
+  await waitForWebServer();
+  async function waitForWebServer() {
+    return retry(fetchStatusPage, {
+      retries: 100,
+      onRetry: (error, attempt) => {
+        console.log(
+          `Attempt ${attempt} - Failed to fetch Status Page: ${error.message}`,
+        );
+      },
+      minTimeout: 100,
+      factor: 1,
+      randomize: false,
+    });
+
+    async function fetchStatusPage() {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      if (response.status !== 200) {
+        throw new Error(`HTTP error ${response.status}`);
+      }
+    }
+  }
+}
+
+export default { waitForAllServices };
