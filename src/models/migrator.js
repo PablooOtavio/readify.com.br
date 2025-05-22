16e0e8e7981cae40347e96137c17748190a76e52
Pablo Otavio <pabloootaviooliveira@gmail.com>
2025-05-22 13:54:06 -0300

    fix: linting

--- src/models/migrator.js
+++ src/models/migrator.js
@@ -19,7 +19,7 @@ async function runMigrations({ dryRun }) {
   } catch (error) {
     throw new ServiceError(
       "An unexpected error occurred while running migrations",
-      error
+      error,
     );
   } finally {
     await dbClient?.end();
