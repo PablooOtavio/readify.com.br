c5563deea28305084e09a3be71713b14fc63573e
Pablo Otavio <pabloootaviooliveira@gmail.com>
2025-05-28 14:33:23 -0300

    feat: implement user creation with unique validation and error handling

---
+++ src/models/user.js
@@ -0,0 +1,52 @@
+import database from "src/infra/database";
+import { ValidationError } from "src/infra/errors";
+
+async function create(data) {
+  await validateUniqueData(data.email, data.username);
+
+  const newUser = await runInsertQuery(data);
+  return newUser;
+
+  async function runInsertQuery(data) {
+    const result = await database.query({
+      text: `
+    INSERT INTO
+        users (username, email, password) 
+     VALUES 
+        ($1, $2, $3)
+        RETURNING *;`,
+      values: [data.username, data.email, data.password],
+    });
+
+    return result.rows[0];
+  }
+  async function validateUniqueData(email, username) {
+    const { rowCount, rows } = await database.query({
+      text: `
+        SELECT
+          CASE
+            WHEN LOWER(email)    = LOWER($1) THEN 'Email'
+            WHEN LOWER(username) = LOWER($2) THEN 'Username'
+          END AS field
+        FROM users
+        WHERE LOWER(email)    = LOWER($1)
+           OR LOWER(username) = LOWER($2)
+        LIMIT 1;
+      `,
+      values: [email, username],
+    });
+
+    if (rowCount > 0) {
+      const field = rows[0].field;
+      throw new ValidationError({
+        message: `${field} already exists.`,
+        action: `Please, use a different ${field}.`,
+      });
+    }
+  }
+}
+
+const user = {
+  create,
+};
+export default user;
